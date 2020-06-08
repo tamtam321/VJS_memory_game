32eea43db5f808e77d7d3fce0eb6d394ad23ec08
Nguyen Thien Tam <tamtam132@gmail.com>
2020-06-08 15:26:16 +0200

    Finished memory game. Note that it has some bugs, but I will fix it later. The bug is those cards which has already out fo the game and have white backgrond still can click on and it will cause to continue the game.

---
+++ app.js
@@ -0,0 +1,129 @@
+document.addEventListener("DOMContentLoaded", () =>
+{
+    // Card options
+    const cardArray = 
+    [
+        {
+            name: "fries",
+            img: "images/fries.png"
+        },
+        {
+            name: "fries",
+            img: "images/fries.png"
+        },
+        {
+            name: "cheeseburger",
+            img: "images/cheeseburger.png"
+        },
+        {
+            name: "cheeseburger",
+            img: "images/cheeseburger.png"
+        },
+        {
+            name: "hotdog",
+            img: "images/hotdog.png"
+        },
+        {
+            name: "hotdog",
+            img: "images/hotdog.png"
+        },
+        {
+            name: "ice-cream",
+            img: "images/ice-cream.png"
+        },
+        {
+            name: "ice-cream",
+            img: "images/ice-cream.png"
+        },
+        {
+            name: "milkshake",
+            img: "images/milkshake.png"
+        },
+        {
+            name: "milkshake",
+            img: "images/milkshake.png"
+        },
+        {
+            name: "pizza",
+            img: "images/pizza.png"
+        },
+        {
+            name: "pizza",
+            img: "images/pizza.png"
+        },
+    ];
+
+    // Shuffle cards
+    cardArray.sort(() => 0.5 - Math.random());
+
+    const grid = document.querySelector(".grid");
+    const resultDisplay = document.querySelector("#result");
+    var cardsChosen = [];
+    var cardsChosenId = [];
+    var cardsWon = [];
+
+    // Create gameboard
+    function createBoard()
+    {
+        for(let i = 0; i < cardArray.length; i++)
+        {
+            var card = document.createElement("img");
+            card.setAttribute("src", "images/blank.png");
+            
+            card.setAttribute("data-id", i);
+
+            card.addEventListener("click", flipCard);
+            grid.appendChild(card);
+        }
+    }
+
+    // Check for matches
+    function checkForMatch()
+    {
+        var cards = document.querySelectorAll("img");
+        const optionOneId = cardsChosenId[0];
+        const optionTwoId = cardsChosenId[1];
+
+        if(cardsChosen[0] === cardsChosen[1])
+        {
+            alert("You found a match");
+            cards[optionOneId].setAttribute("src", "images/white.png");
+            cards[optionTwoId].setAttribute("src", "images/white.png");
+            cardsWon.push(cardsChosen);
+        }
+        else
+        {
+            cards[optionOneId].setAttribute("src", "images/blank.png");
+            cards[optionTwoId].setAttribute("src", "images/blank.png");
+            alert("Sorry, try again");
+        }
+
+        cardsChosen = [];
+        cardsChosenId = [];
+        resultDisplay.textContent = cardsWon.length;
+
+        if(cardsWon.length === cardArray.length/2)
+        {
+            resultDisplay.textContent = "Congratulations! You found them all!";
+        }
+    }
+
+
+    // Card flipping
+    function flipCard()
+    {
+        var cardId = this.getAttribute("data-id");
+
+        cardsChosen.push(cardArray[cardId].name);
+        cardsChosenId.push(cardId);
+        this.setAttribute("src", cardArray[cardId].img);
+
+        if(cardsChosen.length === 2)
+        {
+            // setTimeout: set time when the function will execute. This case it will invoke the checkForMatch function after 500ms.
+            setTimeout(checkForMatch, 500)
+        }
+    }
+
+    createBoard();
+});
